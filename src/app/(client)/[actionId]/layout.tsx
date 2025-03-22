9f83d5ee5218023f482bff8d023f140aa4b7018f
Bernard Kintzing <bernard@kintzing.dev>
2025-03-22 15:04:07 -0600

    Fix notifications and layout UI bugs

--- src/app/(client)/[actionId]/layout.tsx
+++ src/app/(client)/[actionId]/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-screen flex-col items-center overflow-scroll">
-      <div className="w-full max-w-2xl p-4">{children}</div>
+      <div className="w-full max-w-2xl p-4 pb-24">{children}</div>
     </div>
   );
 }
